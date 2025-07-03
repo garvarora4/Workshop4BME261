81f316040860570d6f21fe6e6f6481caa7a085fc
Garv Arora <g25arora@uwaterloo.ca>
2025-07-03 12:13:50 -0400

    This is the first change

---
+++ sketch_jun4a_copy_20250604204029_copy_20250606005037/sketch_jun4a_copy_20250604204029_copy_20250606005037.ino
@@ -0,0 +1,33 @@
+const int ledPin = 6;
+const int buttonPin = 2;
+
+int brightnessLevels[] = {0, 85, 170, 255};  // PWM values for Off, Low, Med, High
+int currentLevel = 0;
+int lastButtonState = LOW;
+bool buttonPressed = false;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  int buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+  // Detect a press (LOW to HIGH transition)
+  if (buttonState == HIGH && lastButtonState == LOW && !buttonPressed) {
+    buttonPressed = true;
+    currentLevel = (currentLevel + 1) % 4;  // Cycle through 0-1-2-3-0
+    analogWrite(ledPin, brightnessLevels[currentLevel]);
+    delay(50);  // debounce delay
+  }
+
+  // Wait until button is released to allow next press
+  if (buttonState == LOW) {
+    buttonPressed = false;
+  }
+
+  lastButtonState = buttonState;
+}
